7634afd65cdab8def51743984a5c4413b2fdde27
Chufretalas <marcobene.contact@gmail.com>
2022-11-06 17:02:19 -0300

    added the basic structure

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+    import "../reset.css"
+    import "../app.css"
+</script>
+
+<slot></slot>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,47 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+</script>
+
+<svelte:head>
+    <title>SadTasks | Home</title>
+</svelte:head>
+
+<main>
+    <h1>SadTasks :( SvelteKit</h1>
+    <section class="form-section">
+        <form action="">
+            <div class="form-group">
+                <label for="title">Title</label>
+                <input name="title" type="text" id="title" />
+            </div>
+            <div class="form-group">
+                <label for="content">Content</label>
+                <input name="content" type="text" id="content" />
+            </div>
+            <button>Save</button>
+        </form>
+    </section>
+    <hr class="separator" />
+    <section class="list-section">
+        <h2>Your tasks</h2>
+        <ul class="task-list">
+            <li>Task</li>
+            <li>Task</li>
+            <li>Task</li>
+            <li>Task</li>
+            <li>Task</li>
+            <li>Task</li>
+        </ul>
+    </section>
+</main>
+
+<style>
+    main {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+    .separator {
+        background-color: var(--accent-color);
+        width: 30vw;
+    }
+</style>
